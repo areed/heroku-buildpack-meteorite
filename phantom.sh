1000a89c75d88f7ccb446fffd6e4f465db46c2b7
apendua <apendua@gmail.com>
2014-10-08 01:15:10 +0200

    Actually we need to update PATH in .profile.d

--- phantom.sh
+++ phantom.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "Updating LD_LIBRARY_PATH in .profile.d";
+echo "Updating PATH and LD_LIBRARY_PATH in .profile.d";
 export LD_LIBRARY_PATH="/app/vendor/phantomjs/lib:${LD_LIBRARY_PATH}";
+export PATH="/app/vendor/phantomjs/bin:${PATH}";
 
 echo "${LD_LIBRARY_PATH}"
+echo "${PATH}"
